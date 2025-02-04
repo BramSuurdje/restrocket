9aa01fedeb7637634a3d5345e4e5285ae1b42a5e
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2025-02-04 19:03:46 +0100

    Add route configurations and utility functions for managing application routes with type safety using Prisma models

---
+++ src/config/routes.ts
@@ -0,0 +1,44 @@
+/*
+  This file contains the route configurations for the application.
+  It is used to define the routes and their permissions for each role.
+  New routes can be added here in a type safe manner. if the model does not exist in the prisma schema,
+  the route cannot be created.
+*/
+
+import type { Prisma } from "@prisma/client";
+import type * as Schemas from "../lib/prisma";
+
+// Add RouteConfig export
+export interface RouteConfig {
+	routeName: string;
+}
+
+export const routeConfigurations: RouteConfigInputType = {
+	User: { routeName: "user" },
+	Session: { routeName: "session" },
+	Hello: { routeName: "hello" },
+	ShalomTestingRoute: { routeName: "shalom-testing-route" },
+} as const;
+
+export type PrismaModels = Prisma.ModelName;
+export type RouteName = Lowercase<PrismaModels>;
+export type SchemaKey = keyof typeof Schemas;
+export type RouteConfigType = Partial<Record<PrismaModels, RouteConfig>>;
+
+// get an arrray of all the route names
+export const routeNames = Object.values(routeConfigurations).map(
+	(route) => route.routeName,
+);
+
+// get the associated route key with the route name
+export const getRouteKey = (routeName: string) => {
+	return Object.keys(routeConfigurations).find(
+		(key) =>
+			routeConfigurations[key as keyof typeof routeConfigurations]?.routeName ===
+			routeName,
+	);
+};
+
+export type RouteConfigInputType = Partial<
+	Record<PrismaModels, Omit<RouteConfig, "schema">>
+>;
