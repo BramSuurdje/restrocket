2aeb2b0c997cd9fb90e0b6ae406940d51c60fa43
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2025-02-05 20:40:02 +0100

    Add dotenv and dotenv-expand for environment variable management; introduce env.ts for validation and configuration

---
+++ src/env.ts
@@ -0,0 +1,49 @@
+/* eslint-disable node/no-process-env */
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import * as path from "node:path";
+import { z } from "zod";
+
+expand(
+	config({
+		path: path.resolve(
+			process.cwd(),
+			process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+		),
+	}),
+);
+
+const EnvSchema = z
+	.object({
+		NODE_ENV: z.string().default("development"),
+		PORT: z.coerce.number().default(3000),
+		LOG_LEVEL: z.enum([
+			"fatal",
+			"error",
+			"warn",
+			"info",
+			"debug",
+			"trace",
+			"silent",
+		]),
+		DATABASE_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string().url(),
+    REDIS_STRING: z.string(),
+    RATE_LIMITER_POINTS: z.coerce.number(),
+    RATE_LIMITER_DURATION: z.coerce.number(),
+	})
+
+export type env = z.infer<typeof EnvSchema>;
+
+// eslint-disable-next-line ts/no-redeclare
+const { data: env, error } = EnvSchema.safeParse(process.env);
+
+if (error) {
+	console.error("❌ Invalid env:");
+	console.error(JSON.stringify(error.flatten().fieldErrors, null, 2));
+	process.exit(1);
+}
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+export default env!;
